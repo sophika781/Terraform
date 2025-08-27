788a04f5cba3082691ad20d1550b286042f26ec6
Sophika Goban <83545279+sophika781@users.noreply.github.com>
2025-08-27 16:55:36 -0400

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "s3-test-bucket-sophika"
-    key = "terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt = true
-  }
-} 
