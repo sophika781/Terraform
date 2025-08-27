fe54da05a44516de2d14ee1730391b7eadf352bc
Sophika <sophika171.g@gmail.com>
2025-08-27 16:22:03 -0400

    Created and imported EC2 instance. Generated SSH key pair and attached to the EC2 instance using Terraform

--- ami-terraform/main.tf
+++ ami-terraform/main.tf
@@ -1,33 +1,33 @@
 provider "aws" {
-	region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_security_group" "server_sg" {
-	description = "Security group for both web servers"
-	ingress {
-		description = "SSH"
-		from_port = 22
-		to_port = 22
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
+  description = "Security group for both web servers"
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-	egress {
-		from_port = 0
-		to_port = 0
-		protocol = "-1"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "dev_ec2" {
-	ami = "ami-00ca32bbc84273381"
-	instance_type = "t3.micro"
-	vpc_security_group_ids = [aws_security_group.server_sg.id]
-	tags = {
-		"Name" = "dev_ec2"
-		"Environment" = "Dev"
-	}
+  ami                    = "ami-00ca32bbc84273381"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.server_sg.id]
+  tags = {
+    "Name"        = "dev_ec2"
+    "Environment" = "Dev"
+  }
 }
 
 resource "aws_ami_from_instance" "image_from_dev" {
@@ -40,11 +40,11 @@ resource "aws_ami_from_instance" "image_from_dev" {
 }
 
 resource "aws_instance" "prod_ec2" {
-  ami = aws_ami_from_instance.image_from_dev.id
-  instance_type = "t3.micro"
+  ami                    = aws_ami_from_instance.image_from_dev.id
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.server_sg.id]
   tags = {
-    "Name" = "prod_ec2"
+    "Name"        = "prod_ec2"
     "Environment" = "Prod"
   }
 
