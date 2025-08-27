e28b0f14f777072146e57e0291dadae6b3f7ffaa
Sophika <sophika171.g@gmail.com>
2025-08-27 16:45:34 -0400

    Created and imported EC2 instance. Generated SSH key pair and attached to the EC2 instance using Terraform

--- ami-terraform/backend.tf
+++ ami-terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "s3-ami-bucket-sophika"
-    key = "terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "s3-ami-bucket-sophika"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
-    encrypt = true
+    encrypt        = true
   }
-} 
+}
