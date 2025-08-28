f83853d7493a9727f881409015da6d502451ccbe
Sophika <sophika171.g@gmail.com>
2025-08-27 20:34:14 -0400

    Creating backend resources and initializing the backend

--- vpc-terraform/backend_resources/state_backend.tf
+++ vpc-terraform/backend_resources/state_backend.tf
@@ -5,6 +5,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "s3_backend_bucket"{
     bucket = "s3-backend-bucket-sophika"
+    acl = "private"
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
