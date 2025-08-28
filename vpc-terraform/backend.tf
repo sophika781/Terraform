9a1a0af9130f2c20f3856639c525f8e9ff22857d
Sophika <sophika171.g@gmail.com>
2025-08-27 21:32:36 -0400

    Creating the VPC

--- vpc-terraform/backend.tf
+++ vpc-terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-        bucket = "s3-backend-bucket-sophika"
-        key = "terraform.tfstate"
-        region = "us-east-1"
-        dynamodb_table = "terraform_lock"
-        encrypt = true
-    }
+  backend "s3" {
+    bucket         = "s3-backend-bucket-sophika"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform_lock"
+    encrypt        = true
+  }
 }
--- vpc-terraform/main.tf
+++ vpc-terraform/main.tf
@@ -3,22 +3,10 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main_vpc" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
-    tags = {
-        Name = "main_vpc"
-    }
-}
-
-resource "aws_subnet" "main_public" {
-
-}
-
-resource "aws_subnet" "main_private" {
-
-}
-
-resource "aws_instance" "public_ec2" {
-
+  tags = {
+    Name = "main_vpc"
+  }
 }
 
