63bddb72d21a7fccfc240dbd53104ad8ed648c80
Sophika <sophika171.g@gmail.com>
2025-08-28 15:14:58 -0400

    Created S3 bucket with versioning and lifecycle configuration

--- cloud-front-terraform/main.tf
+++ cloud-front-terraform/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    region = var.region
+  region = var.region
 }
 
 resource "aws_s3_bucket" "s3-cloud-front-bucket-sophika-2" {
-    bucket = var.s3_cloud_front_bucket
+  bucket = var.s3_cloud_front_bucket
 }
 
 resource "aws_s3_bucket_versioning" "cloud_front_versioning" {
@@ -18,22 +18,22 @@ resource "aws_s3_bucket_lifecycle_configuration" "example" {
   bucket = aws_s3_bucket.s3-cloud-front-bucket-sophika-2.id
 
   rule {
-    id = "s3-cloud-front-transition-delete"
+    id     = "s3-cloud-front-transition-delete"
     status = "Enabled"
 
     filter {
-        prefix = ""
+      prefix = ""
     }
     transition {
-      days= 30
+      days          = 30
       storage_class = "GLACIER"
     }
     transition {
-      days = 120
+      days          = 120
       storage_class = "DEEP_ARCHIVE"
     }
     expiration {
-      days= 365
+      days = 365
     }
 
     noncurrent_version_expiration {
@@ -42,11 +42,11 @@ resource "aws_s3_bucket_lifecycle_configuration" "example" {
 
     noncurrent_version_transition {
       noncurrent_days = 30
-      storage_class = "GLACIER"
+      storage_class   = "GLACIER"
     }
     noncurrent_version_transition {
       noncurrent_days = 120
-      storage_class = "DEEP_ARCHIVE"
+      storage_class   = "DEEP_ARCHIVE"
     }
   }
 }
