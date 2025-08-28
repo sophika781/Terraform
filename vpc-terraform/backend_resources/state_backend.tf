54476802c6fe017b6405a9442e7936c1c1764a8d
Sophika <sophika171.g@gmail.com>
2025-08-27 21:54:02 -0400

    Added private subnet

--- vpc-terraform/backend_resources/state_backend.tf
+++ vpc-terraform/backend_resources/state_backend.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-    region = "us-east-1"
-    alias = "backend_setup"
+  region = "us-east-1"
+  alias  = "backend_setup"
 }
 
-resource "aws_s3_bucket" "s3_backend_bucket"{
-    bucket = "s3-backend-bucket-sophika"
+resource "aws_s3_bucket" "s3_backend_bucket" {
+  bucket = "s3-backend-bucket-sophika"
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
-    bucket = aws_s3_bucket.s3_backend_bucket.id 
+  bucket = aws_s3_bucket.s3_backend_bucket.id
 
-    versioning_configuration {
-        status = "Enabled"
-    }
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "disable_block" {
@@ -34,7 +34,7 @@ resource "aws_s3_bucket_policy" "s3_backend_policy" {
         Sid       = "AllowTerraformStateAccess"
         Effect    = "Allow"
         Principal = "*"
-        Action    = [
+        Action = [
           "s3:GetObject",
           "s3:PutObject",
           "s3:DeleteObject"
@@ -48,12 +48,12 @@ resource "aws_s3_bucket_policy" "s3_backend_policy" {
 
 
 resource "aws_dynamodb_table" "terraform_lock" {
-    name = "terraform_lock"
-    billing_mode   = "PAY_PER_REQUEST"
-    hash_key       = "LockID"
+  name         = "terraform_lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
