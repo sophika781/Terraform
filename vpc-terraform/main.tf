5b0aa45bda2a49b9f63c7d3090986901bfd9067b
Sophika <sophika171.g@gmail.com>
2025-08-27 22:22:20 -0400

    added security group

--- vpc-terraform/main.tf
+++ vpc-terraform/main.tf
@@ -29,28 +29,29 @@ resource "aws_subnet" "private_vpc" {
 }
 
 resource "aws_security_group" "server_sg" {
-    description = "Security group for the EC2 instance"
+  description = "Security group for the EC2 instance"
 
-    ingress {
-        description = "SSH"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        description = "HTTP"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
