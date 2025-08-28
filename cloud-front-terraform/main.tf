25b7f8df8a37195acd68d3146632ca30cad451b1
Sophika <sophika171.g@gmail.com>
2025-08-28 16:11:20 -0400

    Created the cloud front distribution, the bucket policy that allows CloudFront to control access to the bucket and origin access control resources

--- cloud-front-terraform/main.tf
+++ cloud-front-terraform/main.tf
@@ -50,3 +50,72 @@ resource "aws_s3_bucket_lifecycle_configuration" "example" {
     }
   }
 }
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "my-oac"
+  description                       = "OAC for S3 bucket access"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name              = aws_s3_bucket.s3-cloud-front-bucket-sophika-2.bucket_regional_domain_name
+    origin_id                = "s3-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+}
+
+resource "aws_s3_bucket_policy" "cloud_front_bucket_policy" {
+  bucket = aws_s3_bucket.s3-cloud-front-bucket-sophika-2.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontAccess"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.s3-cloud-front-bucket-sophika-2.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
+          }
+        }
+      }
+    ]
+  })
+}
