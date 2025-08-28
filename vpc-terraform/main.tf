e0c137192d34aa354d78fd734576f42f2e144688
Sophika <sophika171.g@gmail.com>
2025-08-27 22:56:33 -0400

    creating ec2 instance and installing nginx through bootstrap script

--- vpc-terraform/main.tf
+++ vpc-terraform/main.tf
@@ -55,8 +55,18 @@ resource "aws_security_group" "server_sg" {
   }
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_instance" "server_ec2" {
-  ami                    = "ami-0b016c703b95ecbe4"
+  ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.server_sg.id]
   subnet_id              = aws_subnet.public_subnet.id
