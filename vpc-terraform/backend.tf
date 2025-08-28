dc13f0201bc46bf326fde9d0b2c2bc2e8d05c134
Sophika <sophika171.g@gmail.com>
2025-08-27 20:16:38 -0400

    Creating backend resources and initializing the backend

--- vpc-terraform/backend.tf
+++ vpc-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "s3_backend_bucket"
+        bucket = "s3-backend-bucket"
         key = "terraform.tfstate"
         region = "us-east-1"
         dynamodb_table = "terraform_lock"
