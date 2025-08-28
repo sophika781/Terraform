505db4c788f44b963544fd295692a5f098e076cf
Sophika <sophika171.g@gmail.com>
2025-08-27 21:39:10 -0400

    Creating the public subnet

--- vpc-terraform/main.tf
+++ vpc-terraform/main.tf
@@ -6,7 +6,15 @@ resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "main_vpc"
+    "Name" = "main_vpc"
   }
 }
 
+resource "aws_subnet" "public_vpc" {
+    vpc_id= aws_vpc.main_vpc.id
+    cidr_block = "10.0.0.0/24"
+
+    tags = {
+        "Name" = "public_vpc"
+    }
+}
