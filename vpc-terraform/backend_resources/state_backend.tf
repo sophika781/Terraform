d7b62ae5a4a37085ef4e1828bc1d5d9b6eb82d18
Sophika <sophika171.g@gmail.com>
2025-08-27 20:08:21 -0400

    Creating backend resources and initializing the backend

---
+++ vpc-terraform/backend_resources/state_backend.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+    region = "us-east-1"
+    alias = "backend_setup"
+}
+
+resource "aws_s3_bucket" "s3_backend_bucket"{
+    bucket = "s3-backend-bucket-sophika"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+    bucket = aws_s3_bucket.s3_backend_bucket.id 
+
+    versioning_configuration {
+        enabled = true
+    }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+    name = "terraform_lock"
+    billing_mode   = "PAY_PER_REQUEST"
+    hash_key       = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
