d84b71e4faa76469470cd0d8a1572b28a632aa7f
Sophika <sophika171.g@gmail.com>
2025-09-01 15:46:17 -0400

    fixed test instance

--- spring-boot-terraform/main.tf
+++ spring-boot-terraform/main.tf
@@ -230,10 +230,10 @@ resource "aws_ami_from_instance" "app_ami" {
 }
 
 resource "aws_instance" "test_ec2" {
-  subnet_id              = aws_subnet.public_1.id
-  ami                    = aws_ami_from_instance.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.server_sg.id]
+  subnet_id                   = aws_subnet.public_1.id
+  ami                         = aws_ami_from_instance.app_ami.id
+  instance_type               = var.instance_type
+  vpc_security_group_ids      = [aws_security_group.server_sg.id]
   associate_public_ip_address = true
   tags = {
     "Name" = "Second Server"
