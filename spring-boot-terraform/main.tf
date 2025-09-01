d1a33c6272f78731211d78a50f7d359b509c7cd1
Sophika <sophika171.g@gmail.com>
2025-09-01 19:16:35 -0400

    added launch template, target groups, alb

--- spring-boot-terraform/main.tf
+++ spring-boot-terraform/main.tf
@@ -82,10 +82,10 @@ resource "aws_security_group" "server_sg" {
     cidr_blocks = [var.ingress_cidr_block]
   }
   ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    description     = "HTTP"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
   }
   ingress {
