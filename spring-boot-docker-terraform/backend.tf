8476540efb69f9ad0c435ce3bb0e73a818c95624
Sophika <sophika171.g@gmail.com>
2025-09-03 15:59:45 -0400

    formatted

--- spring-boot-docker-terraform/backend.tf
+++ spring-boot-docker-terraform/backend.tf
@@ -1,10 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-backend-bucket-docker-sophika"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform_locks_docker"
-    encrypt        = true
+    encrypt = true
   }
 }
 
