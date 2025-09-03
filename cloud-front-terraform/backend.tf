f3f476391f36979d620092d0ad52b4b7228cd46f
Sophika Goban <83545279+sophika781@users.noreply.github.com>
2025-09-03 16:34:29 -0400

    Update backend.tf

--- cloud-front-terraform/backend.tf
+++ cloud-front-terraform/backend.tf
@@ -1,9 +1,5 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-backend-bucket-sophika"
-    key            = "terraform.tfstate"
-    dynamodb_table = "terraform-locks"
-    region         = "us-east-1"
     encrypt        = true
 
   }
